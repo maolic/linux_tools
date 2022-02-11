8653bca9d516ef53a97f1eb205755f765be61046
Shen <maolic@tom.com>
2022-02-11 13:35:05 +0800

    1. 修改版本错误

--- install_java.sh
+++ install_java.sh
@@ -5,7 +5,7 @@
 #	Version: 1.0
 #	Author: MLC
 #=================================================
-version="1.1"
+version="1.0"
 path="/usr/local/java"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
