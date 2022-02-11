ffe6d7deb3da82abd45bb6f7ba017763593cb585
Shen <maolic@tom.com>
2022-02-11 11:26:38 +0800

    1. 添加Java安装一键脚本 2. 更新说明文档

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ### Nginx一键安装
 
+- 支持系统：CentOS 7+, Debian 9+, Ubuntu 16+
+- 安装路径：/usr/local/nginx
+
 **2022年2月10日更新** v1.0.4
 1. 支持自定义版本的离线安装，将nginx源码包上传到指定目录即可。
 2. 支持升级至1.21.6版本
@@ -11,8 +14,19 @@
  2. 支持升级至1.21.5版本
 
 目前支持CentOS 7+, Debian 9+, Ubuntu 16+系统，请确保服务器80和443端口不被占用。
-安装路径：/usr/local/nginx
-
 ```
-wget https://raw.githubusercontent.com/maolic/linux_tools/main/install_nginx.sh && bash install_nginx.sh
+wget https://raw.githubusercontent.com/maolic/linux_tools/main/install_nginx.sh
+bash install_nginx.sh
+```
+
+### JDK一键安装
+
+- 支持系统：CentOS 7+, Debian 9+, Ubuntu 16+
+- 已测试版本：JDK8、JDK17
+- 安装路径：/usr/local/java
+
+请先前往Oracle官网下载对应版本JDK压缩包后上传至服务器，再运行此脚本。
+```
+wget https://raw.githubusercontent.com/maolic/linux_tools/main/install_java.sh
+source install_java.sh
 ```
---
+++ install_java.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#=================================================
+#	System Required: CentOS 7+, Debian 9+, Ubuntu 16+
+#	Description: Java JDK 离线一键安装脚本
+#	Version: 1.0
+#	Author: MLC
+#=================================================
+version="1.1"
+path="/usr/local/java"
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+clear
+echo
+echo "#############################################################"
+echo "# System Required: CentOS 7+, Debian 9+, Ubuntu 16+         #"
+echo "# Description: Java JDK 离线一键安装脚本                    #"
+echo "# Author: MLC <mlc@tom.com>                                 #"
+echo "# Github: https://github.com/maolic                         #"
+echo "#############################################################"
+echo
+[[ $EUID != 0 ]] && echo -e "${Error} 当前非ROOT账号(或没有ROOT权限)，无法继续操作，请更换ROOT账号或使用 ${Green_background_prefix}sudo su${Font_color_suffix} 命令获取临时ROOT权限（执行后可能会提示输入当前账号的密码）。" && exit 1
+echo
+echo -e "${Tip} 请前往Oracle官网下载对应版本tar.gz结尾压缩包后上传至服务器。\n 下载地址：https://www.oracle.com/java/technologies/downloads/"
+read -e -p " 请输入完整JDK安装包路径和名称:" jdk_path
+[[ ! -s "$jdk_path" ]] && echo -e "${Error} JDK 文件不存在，安装失败 !" && exit 1
+echo " 即将安装的包路径：$jdk_path"
+mkdir ${path}
+tar -zxvf ${jdk_path} -C ${path} --strip-components 1
+
+echo '' >> /etc/profile
+echo 'export JAVA_HOME='${path} >> /etc/profile
+echo 'export JRE_HOME=${JAVA_HOME}/jre' >> /etc/profile
+echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> /etc/profile
+echo 'export PATH=${JAVA_HOME}/bin:$PATH' >> /etc/profile
+sleep 1s
+source /etc/profile
+
+echo
+echo '—————— 版本信息 ——————'
+java -version
+echo '—————— 版本信息 ——————'
+echo
+echo -e "${Info} 安装完成，如果出现Java版本信息不为空则安装成功，如果未生效则手动输入命令执行生效 'source /etc/profile'"
+echo
